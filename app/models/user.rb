defbc5768c6352063fbbdcfd85bb769563e570da
hakraoul <haknjr11@gmail.com>
2024-10-20 01:52:55 +0700

    close #11 add authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :budgets, dependent: :destroy
+
+  def self.from_omniauth(auth)
+    where(email: auth.info.email).first_or_initialize do |user|
+      user.username = auth.info.name
+      user.email = auth.info.email
+      user.password = SecureRandom.hex
+    end
+  end
 end
